6a38820358a0e9f51cc16a96278f24289cb451cb
ElyssaW <ejwinch1@gmail.com>
2021-05-08 03:40:16 -0500

    Call to action edit again

--- pages/Components/CallToAction.js
+++ pages/Components/CallToAction.js
@@ -9,7 +9,7 @@ export default function CallToAction() {
             <div className='about-me-div'>
                 <p className='about-me'>I'm a full-stack developer and San Antonio native with a drive to create and a hunger to learn. I've got a passion for narrative, an eye for aesthetics, and a curisoity that is never satisfied, alongside an arsenal of skills and smarts that can push limits and solve problems in a snap.</p>
                 
-                <p className='about-me'>When I'm not coding, I love to draw, cook, play video games, or run a game of DnD with friends. On top of programming, I'm also a published writer, a freelance illustrator, a hobbyist game developer, and an irrepressible baker. I enjoy inunduating people with baked goods and taking pictures of my cat.</p>
+                <p className='about-me'>Outside of the tech world, I can be caught drawing, cooking, playing video games, or running a game of DnD with friends. I'm also a published writer, a freelance illustrator, a hobbyist game developer, and an irrepressible baker. I enjoy inunduating people with baked goods and taking pictures of my cat.</p>
             </div>
         </div>
     )
