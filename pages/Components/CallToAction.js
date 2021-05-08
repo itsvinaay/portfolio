fdd7b984481deabe7b6bb4ade1b9d5ae1efd3112
ElyssaW <ejwinch1@gmail.com>
2021-05-08 03:29:57 -0500

    Edited call to action

--- pages/Components/CallToAction.js
+++ pages/Components/CallToAction.js
@@ -7,9 +7,9 @@ export default function CallToAction() {
             <h1 className='section-title call-to-action-title'>Collaborate With Me</h1>
 
             <div className='about-me-div'>
-                <p className='about-me'>I'm a full-stack developer and San Antonio native, delighted to create and always hungry to learn. I've got a passion for narrative, an eye for aesthetics, and a curisoity that is never satisfied, alongside an arsenal of skills and smarts that can push limits and solve problems in a snap.</p>
+                <p className='about-me'>I'm a full-stack developer and San Antonio native with a drive to create and a hunger to learn. I've got a passion for narrative, an eye for aesthetics, and a curisoity that is never satisfied, alongside an arsenal of skills and smarts that can push limits and solve problems in a snap.</p>
                 
-                <p className='about-me'>When I'm not coding, I love to draw, cook, play video games, or enjoy a night of DnD with friends. On top of programming, I'm also a published writer, a freelance illustrator, a hobbyist game developer, and an incorrigble baker. I enjoy inunduating people with baked goods and taking pictures of my cat.</p>
+                <p className='about-me'>When I'm not coding, I love to draw, cook, play video games, or run a game of DnD with friends. On top of programming, I'm also a published writer, a freelance illustrator, a hobbyist game developer, and an irrepressible baker. I enjoy inunduating people with baked goods and taking pictures of my cat.</p>
             </div>
         </div>
     )
